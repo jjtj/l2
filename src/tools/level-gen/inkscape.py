e17b2dc76fae3064335c7ccdb8a4d2351658e027
jjtj <jaeho@outlook.com>
2012-08-29 13:47:09 -0700

    CHANGED: level generation script

--- src/tools/level-gen/inkscape.py
+++ src/tools/level-gen/inkscape.py
@@ -2,24 +2,59 @@
 import os
 import sys
 
-INKSCAPE_PATH ="\"C:/Program Files (x86)/Inkscape/inkscape.exe\" " 
+INKSCAPE_PATH ="\"C:/Program Files (x86)/Inkscape/inkscape.exe\" "
+TEMP_QUERY_FILE = "inter.txt" 
 
-class Inkscape:
-    def __init__(self, svgfn):
-        self.svgfn = svgfn
-        
-    def exportPage(self):
-        
-        pass
+def queryAllObjects(svgfn):
     
-    def execute(self, cmd):
-        os.system(cmd)
+    print ''
+    print 'Query inkscape objects from ' + svgfn
+    print '----------------------------'
+    print 'Working...'
+    print ''
     
-    def buildQueryCmd(self, params, query):
-        cmd =  INKSCAPE_PATH + params + " " + self.svgfn + " > inter.txt"
-        return cmd
+    cmd = _buildQueryCmd(svgfn, "-S")
+    execute(cmd)
     
-    def buildExportCmd(self, params):
-        cmd = INKSCAPE_PATH + params + " " + self.svgfn
-        return cmd
-        
+    entities = []
+    
+    lines = open(TEMP_QUERY_FILE).readlines()
+    for ln in lines:
+        obj = _parseObjectLine(ln)
+        if obj <> None:
+            entities.append(obj)
+    
+    print 'Total %d entities found...' % (len(entities))
+    return entities
+            
+
+def exportPage(svgfn):
+    pass
+
+def execute(cmd):
+    os.system(cmd)
+    
+def _buildQueryCmd(svgfn, params):
+    cmd = INKSCAPE_PATH + params + " " + svgfn + " > " + TEMP_QUERY_FILE
+    return cmd
+
+def _buildExportCmd(svgfn, params):
+    cmd = INKSCAPE_PATH + params + " " + svgfn
+    return cmd 
+
+
+def _toInt(s):
+    return int(float(s))    
+
+def _parseObjectLine(ln):
+    p = ln.split(',')
+    if len(p) <> 5:
+        return None
+    
+    return dict(id=p[0],    \
+                x=_toInt(p[1]),\
+                y=_toInt(p[2]),\
+                w=_toInt(p[3]),\
+                h=_toInt(p[4]))
+    
+    
--- src/tools/level-gen/level-gen.py
+++ src/tools/level-gen/level-gen.py
@@ -1,8 +1,80 @@
-
 import sys
 import os
 import utils
+import inkscape
+import re
 
+def groupLevelObjects(objects):
+    
+    print 'Grouping level objects...'
+        
+    prog = re.compile('level-(\d+)-.+');
+        
+    group = dict()
+    
+    for obj in objects:
+        result = prog.match(obj['id'])
+        if result <> None:
+            level = result.group(1)
+            isIn = level in group
+            
+            if isIn == False:
+                group[level] = []
+                
+            group[level].append(obj)
+            
+    return group
+
+def isCheckPoint(id):
+    result = re.match('level-(\d+)-check-point-(\d+)', id)
+    if result == None:
+        return False
+    
+    return True
+
+def isTerrain(id):
+    result = re.match('level-(\d+)-terrain', id)
+    return result <> None
+
+def isTitle(id):
+    result = re.match('level-(\d+)-title', id)
+    return result <> None
+
+    
+def createLevel(lvlNum, objects):
+    level = dict(levelNum=lvlNum,   \
+                 checkPoints = [],  \
+                 terrain=None,
+                 title=None)
+    
+    level['levelNum'] = int(lvlNum)
+    for obj in objects:
+        objid = obj['id']
+        if isCheckPoint(objid):
+            level['checkPoints'].append(obj)
+        elif isTerrain(objid):
+            level['terrain'] = obj
+        elif isTitle:
+            level['title'] = obj
+        else:
+            print 'Unexpected object: ' + objid
+    
+    return level
+        
+    
+def begin(inputfn, outputfolder):
+    objects = inkscape.queryAllObjects(inputFn)    
+    groups = groupLevelObjects(objects)
+    
+    print 'Collect & creating level data...'
+    levels = []
+    for v in groups:
+        lvl = createLevel(v, groups[v])
+        levels.append(lvl)
+    
+    print levels
+        
+    
 if __name__ == "__main__":
     if len(sys.argv) <> 3:
         print "Usage: level-gen.py [input-svg-file] [output-folder]"
@@ -10,13 +82,14 @@ if __name__ == "__main__":
         
     inputFn = os.path.abspath(sys.argv[1])
     outputFolder = os.path.abspath(sys.argv[2])
+    utils.ensureDir(outputFolder)
     
     print 'Level generator:'
     print '----------------'
     print 'Input Svg File: ' + inputFn
     print 'Output Folder: ' + outputFolder
     
-    utils.ensureDir(outputFolder)
+    begin(inputFn, outputFolder)
     
     
     
